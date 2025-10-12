d4f3729191e27fb2df8439f8a62f1b2bad02f505
patryk-pi <patryk.piasecki1@gmail.com>
2025-10-12 09:15:30 +0200

    loops excercises

--- Day2/Day2_warmup.py
+++ Day2/Day2_warmup.py
@@ -10,3 +10,18 @@ print(slowo.capitalize())
 print(slowo.replace("m", "M", 1))
 print(napis.lower())
 print(len(lista_napis))
+
+### PETLE
+
+age = int(input("Ile masz lat? "))
+
+if age <= 0:
+    print("Zły wiek")
+elif age < 18:
+    print(f"Masz {age} lat")
+    print(f"Będziesz dorosły za {18 - age} lat")
+else:
+    print(f"Masz {age} lat")
+    print("Jesteś pełnoletni")
+
+print("Koniec programu")
