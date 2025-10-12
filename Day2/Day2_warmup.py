ec3bf998a322d93121ccda9dd84abd7e60ef7e35
patryk-pi <patryk.piasecki1@gmail.com>
2025-10-12 09:36:25 +0200

    for and while true loops - credit card validator

--- Day2/Day2_warmup.py
+++ Day2/Day2_warmup.py
@@ -13,15 +13,43 @@ print(len(lista_napis))
 
 ### PETLE
 
-age = int(input("Ile masz lat? "))
+# age = int(input("Ile masz lat? "))
+#
+# if age <= 0:
+#     print("Zły wiek")
+# elif age < 18:
+#     print(f"Masz {age} lat")
+#     print(f"Będziesz dorosły za {18 - age} lat")
+# else:
+#     print(f"Masz {age} lat")
+#     print("Jesteś pełnoletni")
+#
+# print("Koniec programu")
 
-if age <= 0:
-    print("Zły wiek")
-elif age < 18:
-    print(f"Masz {age} lat")
-    print(f"Będziesz dorosły za {18 - age} lat")
-else:
-    print(f"Masz {age} lat")
-    print("Jesteś pełnoletni")
+# Walidator karty do bankomatu
 
-print("Koniec programu")
+poprawny_pin = 1234
+
+# for i in range(3):
+#     wprowadzony_pin = int(input("Podaj PIN: "))
+#     if wprowadzony_pin == poprawny_pin:
+#         print(poprawny_pin, "Zapraszam do wypłaty gotówki")
+#         break
+#     else:
+#         print(f"Pin niepoprawny, zostało {2 - i} prób")
+#         if i == 2:
+#             print("Karta zablokowana")
+
+liczba_prob = 3
+
+while True:
+    wprowadzony_pin = (input("Podaj PIN: "))
+    if wprowadzony_pin == poprawny_pin:
+        print(poprawny_pin, "Zapraszam do wypłaty gotówki")
+        break
+    else:
+        liczba_prob -= 1
+        print(f"Pin niepoprawny, zostało {liczba_prob}")
+        if liczba_prob == 0:
+            print("Karta zablokowana")
+            break
