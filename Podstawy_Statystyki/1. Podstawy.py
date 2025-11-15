2c0db2a57a6f46355b91085c2dd463874cd3f082
patryk-pi <patryk.piasecki1@gmail.com>
2025-11-15 09:22:20 +0100

    basic data preparation

---
+++ Podstawy_Statystyki/1. Podstawy.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+
+# DANE Z ZADANIA
+wyniki = np.array([65, 70, 75, 80, 80, 85, 85, 85, 90, 95, 100, 150])
