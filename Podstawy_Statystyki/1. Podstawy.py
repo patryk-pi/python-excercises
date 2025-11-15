49a9787be764013efb8fabb2ef628a0632179a0c
patryk-pi <patryk.piasecki1@gmail.com>
2025-11-15 11:17:28 +0100

    kurtoza i skosnosc

--- Podstawy_Statystyki/1. Podstawy.py
+++ Podstawy_Statystyki/1. Podstawy.py
@@ -78,3 +78,14 @@ print(df.info())
 
 print("\nPięć pierwszych wierszy próbki")
 print(df["wynik"].describe().round(2))
+
+# Dodatkowe statystyki
+print(f"\nSkośność:         {df["wynik"].skew(): .4f}")
+print(f"Kurtoza:           {df["wynik"].kurtosis():.4f}")
+
+if df["wynik"].skew() > 0:
+    print("Rozkład jest skośny prawostronnie (prawy ogon dłuższy)")
+elif df["wynik"].skew() < 0:
+    print("Rozkład jest skośny lewostronnie (lewy ogon dłuższy)")
+else:
+    print("Rozkład jest symetryczny")
