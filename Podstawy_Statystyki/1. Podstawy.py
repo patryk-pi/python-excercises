bb40ef2ea2011d8dddcb5e2fe91d3064729361c3
patryk-pi <patryk.piasecki1@gmail.com>
2025-11-15 09:40:14 +0100

    basic statistics: average, median, mode

--- Podstawy_Statystyki/1. Podstawy.py
+++ Podstawy_Statystyki/1. Podstawy.py
@@ -6,3 +6,16 @@ from scipy import stats
 
 # DANE Z ZADANIA
 wyniki = np.array([65, 70, 75, 80, 80, 85, 85, 85, 90, 95, 100, 150])
+
+
+def separator (text):
+    print("=" * 50, text, "=" * 50, sep = "\n")
+
+
+separator("STATYSTYKA OPISOWA - PODSTAWOWE MIARY")
+
+print("1. MIARY TENDENCJI CENTRALNEJ")
+print("=" * 40)
+print(f"Średnia arytmetyczna: {np.mean(wyniki): .2f} pkt")
+print(f"Mediana:              {np.median(wyniki): .2f} pkt")
+print(f"Moda:                 {stats.mode(wyniki, keepdims = True).mode[0]} pkt")
